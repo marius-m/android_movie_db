5c21c4d4d13a1d65a79fd737099ff990dd74427f
Marius Merkevičius <marius.m@ito.lt>
2015-07-26 16:34:00 +0300

    Tested out networking of movie database. Added networking libraries.

--- app/src/main/java/lt/mm/moviedb/SearchActivity.java
+++ app/src/main/java/lt/mm/moviedb/SearchActivity.java
@@ -2,10 +2,17 @@ package lt.mm.moviedb;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class SearchActivity extends ActionBarActivity {
@@ -20,6 +27,32 @@ public class SearchActivity extends ActionBarActivity {
         AutoCompleteTextView textView = (AutoCompleteTextView)
                 findViewById(R.id.input_search);
         textView.setAdapter(adapter);
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.themoviedb.org/3/"+"search/movie?api_key=f13d3cddb56308882dd225a7e06c92d1&query=terminator";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        // mTextView.setText("Response is: "+ response.substring(0,500));
+                        Log.e("asdc", "Response: "+response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // mTextView.setText("That didn't work!");
+                Log.e("asdc", "Response: " + error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        queue.start();
     }
 
     private static final String[] COUNTRIES = new String[] {
