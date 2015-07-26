3f3aea10873908ca2fae2fc6cc01f9eedd85d3f7
Marius Merkevičius <marius.m@ito.lt>
2015-07-26 15:48:10 +0300

    Added template layouts for user input in search window. Added mock info for finding out how autocomplete view works.

--- app/src/main/java/lt/mm/moviedb/SearchActivity.java
+++ app/src/main/java/lt/mm/moviedb/SearchActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 
 public class SearchActivity extends ActionBarActivity {
@@ -13,7 +15,15 @@ public class SearchActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.input_search);
+        textView.setAdapter(adapter);
     }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 
 }
